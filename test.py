6ffdf06609ddc3445ffaaa226f75a1b5d7949abf
rukesh1212 <rukeshrok143@gmail.com>
2026-07-21 22:25:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,70 @@
+# nums=int(input("Enter the number:"))
+# if nums>0:
+#     print("positive")
+# elif nums<0:
+#     print("negitive")
+# else:
+#     print("0")
+
+
+# a=int(input("enter first sub marks"))
+# b=int(input("enter second sub marks"))
+# c=int(input("enter third sub marks"))
+# stdmarks=[a,b,c]
+# for i in stdmarks:
+#     if i>=40:
+#         print("pass")
+#     else:
+#         print("fail")
+
+# year=int(input("Enter the year"))
+# if( year%4==0 and year%100!=0) or (year%400==0):
+#     print("lep year")
+# else:
+#     print("not a lepiar")
+
+# for i in range(1,51):
+#     if i%2==0:
+#         print(i,end=' ')
+
+# n=int(input("Enter the starting number"))
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+# n=5
+# i=1
+# for i in range(1,11):
+#     print(f'{n} x {i}={n*i}')
+
+# def is_even(number):
+#     if number%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# is_even(1)
+
+# def largest_of_three(a, b, c):
+#     if a>b:
+#         print("a is largest")
+#     elif b>c:
+#         print("b is largest")
+#     elif c>a:
+#         print("c is largest")
+#     else:
+#         print("all are equal")
+# largest_of_three(10,20,30)
+
+# def count_vowels(text):
+#     for char in text:
+#         if char in "aeiou":
+#             print(f"{char}")
+# count_vowels(text='rukesh')
+
+# numbers = [3, 8, 12, 5, 7, 10, 15]
+# print(even:=[i for i in numbers if i%2==0 ])
+
+
+# words = ["python", "java", "c", "javascript", "go"]
+# print(final:={word: len(word) for word in words})
